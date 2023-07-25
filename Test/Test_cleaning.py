9450ec3e94eaccd05a7f7bd9d7eea0eddd6085cc
Asif Samir <64280545+asifsamir@users.noreply.github.com>
2023-07-25 20:52:58 -0300

    dataset some update

--- Test/Test_cleaning.py
+++ Test/Test_cleaning.py
@@ -1,44 +1,11 @@
 import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
-# nltk.download('punkt')
-# nltk.download('stopwords')
-
-def clean_text(text):
-    # Replace newlines with spaces
-    text = text.replace('\n', ' ')
-
-    # Tokenize the text into words
-    words = word_tokenize(text)
-
-    print(words)
-
-    # Remove stop words
-    stop_words = set(stopwords.words('english'))
-    words_without_stopwords = [word for word in words if word.lower() not in stop_words]
-
-    # Remove words that are only numbers
-    words_cleaned = [word for word in words_without_stopwords if not word.isdigit()]
-
-    # Join the words back into a cleaned sentence
-    cleaned_text = ' '.join(words_cleaned)
-
-    # Preserve certain punctuations
-    preserved_punctuations = ['"', "'", "-"]  # Add any other punctuations you want to preserve
-    for punctuation in preserved_punctuations:
-        cleaned_text = cleaned_text.replace(f" {punctuation} ", f"{punctuation} ")
-
-    return cleaned_text
+def remove_number_tokens(text):
+    # Use regular expression to remove tokens made up of only numbers
+    pattern = r'\b[0-9]+\b'  # Matches one or more digits (numbers) only
+    return re.sub(pattern, '', text)
 
 # Example usage:
-text = """
-NLTK is a powerful library for \"natural language processing\".
-It provides tools for text processing tasks like tokenization,
-removing stop words, and more!
-"""
-
-cleaned_text = clean_text(text)
-print(cleaned_text)
-
+input_text = "Hello 123 World! This is a test. 45678"
+output_text = remove_number_tokens(input_text)
+print(output_text)  # Output: "Hello  World! This is a test. "
